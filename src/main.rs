e6bddd076e101c013c17ac5b1382d5f2e45bbb64
Sean Cross <sean@xobs.io>
2017-11-12 19:02:58 +0800

    cargo: release 1.3.16
    This version quits properly, and has an adjustable "repeat" counter.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,10 @@ extern crate elf;
 extern crate clap;
 use clap::{App, Arg};
 
-use std::io::prelude::*;
 use std::fs::File;
+use std::io::prelude::*;
+use std::sync::{Arc, Mutex, Condvar};
+use std::thread;
 
 const DEFAULT_SAMPLE_RATE: f64 = 44100.0;
 
@@ -20,6 +22,7 @@ fn do_modulation(
     os_update: bool,
     version: controller::ProtocolVersion,
     play_file: bool,
+    repeats: u32,
 ) -> std::io::Result<()> {
     let mut output_sample_rate = DEFAULT_SAMPLE_RATE;
 
@@ -77,13 +80,15 @@ fn do_modulation(
     };
     let mut audio_data: Vec<f64> = vec![];
 
-    controller.encode(&input_data, &mut audio_data, data_rate);
-    let mut pilot_controller = controller::Controller::new(output_sample_rate, os_update, version);
-    pilot_controller.pilot(&mut audio_data, data_rate);
-    controller.encode(&input_data, &mut audio_data, data_rate);
+    for _ in 0..repeats {
+        controller.encode(&input_data, &mut audio_data, data_rate);
+        let mut pilot_controller = controller::Controller::new(output_sample_rate, os_update, version);
+        pilot_controller.pilot(&mut audio_data, data_rate);
+    }
 
     if play_file {
-        let mut audio_data_pos = 0;
+        let file_end_signal = Arc::new((Mutex::new(0), Condvar::new()));
+
         let endpoint = cpal::default_endpoint().expect("Failed to get default endpoint");
         let format = endpoint
             .supported_formats()
@@ -97,46 +102,71 @@ fn do_modulation(
         let voice_id = event_loop.build_voice(&endpoint, &format).unwrap();
         event_loop.play(voice_id);
 
-        // Produce a sinusoid of maximum amplitude.
-        let mut next_value = || {
-            if audio_data_pos > audio_data.len() {
-                panic!("Ran out of audio data");
-            }
-            let val = audio_data[audio_data_pos];
-            audio_data_pos = audio_data_pos + 1;
-            val as f32
-        };
+        let thread_mutex = file_end_signal.clone();
+        let audio_data_len = audio_data.len();
 
-        event_loop.run(move |_, buffer| {
-            match buffer {
-                cpal::UnknownTypeBuffer::U16(mut buffer) => {
-                    for sample in buffer.chunks_mut(format.channels.len()) {
-                        let value = ((next_value() * 0.5 + 0.5) * std::u16::MAX as f32) as u16;
-                        for out in sample.iter_mut() {
-                            *out = value;
-                        }
-                    }
-                }
-
-                cpal::UnknownTypeBuffer::I16(mut buffer) => {
-                    for sample in buffer.chunks_mut(format.channels.len()) {
-                        let value = (next_value() * std::i16::MAX as f32) as i16;
-                        for out in sample.iter_mut() {
-                            *out = value;
-                        }
-                    }
-                }
-
-                cpal::UnknownTypeBuffer::F32(mut buffer) => {
-                    for sample in buffer.chunks_mut(format.channels.len()) {
-                        let value = next_value();
-                        for out in sample.iter_mut() {
-                            *out = value;
-                        }
+        thread::spawn(move || {
+            // Produce a sinusoid of maximum amplitude.
+            let next_value = || {
+                let &(ref num, ref _cvar) = &*thread_mutex;
+                {
+                    let mut audio_data_pos = num.lock().unwrap();
+                    if *audio_data_pos >= audio_data.len() {
+                        0.0 as f32
+                    } else {
+                        let val = audio_data[*audio_data_pos];
+                        *audio_data_pos = *audio_data_pos + 1;
+                        //cvar.notify_one();
+                        val as f32
                     }
                 }
             };
+
+            event_loop.run(move |_, buffer| {
+                match buffer {
+                    cpal::UnknownTypeBuffer::U16(mut buffer) => {
+                        for sample in buffer.chunks_mut(format.channels.len()) {
+                            let value = ((next_value() * 0.5 + 0.5) * std::u16::MAX as f32) as u16;
+                            for out in sample.iter_mut() {
+                                *out = value;
+                            }
+                        }
+                    }
+
+                    cpal::UnknownTypeBuffer::I16(mut buffer) => {
+                        for sample in buffer.chunks_mut(format.channels.len()) {
+                            let value = (next_value() * std::i16::MAX as f32) as i16;
+                            for out in sample.iter_mut() {
+                                *out = value;
+                            }
+                        }
+                    }
+
+                    cpal::UnknownTypeBuffer::F32(mut buffer) => {
+                        for sample in buffer.chunks_mut(format.channels.len()) {
+                            let value = next_value();
+                            for out in sample.iter_mut() {
+                                *out = value;
+                            }
+                        }
+                    }
+                };
+            });
         });
+        println!("Thread is now playing");
+
+        loop {
+            {
+                let &(ref num, ref _cvar) = &*file_end_signal;
+                let offset = num.lock().unwrap();
+                if *offset >= audio_data_len {
+                    use std::process;
+                    process::exit(0);
+                }
+            }
+            use std::time::Duration;
+            thread::park_timeout(Duration::from_millis(250));
+        }
     } else {
         let mut output: Vec<i16> = Vec::new();
         for sample in audio_data {
@@ -155,7 +185,7 @@ fn do_modulation(
 
 fn main() {
     let matches = App::new("Love-to-Code Program Modulator")
-        .version("1.2")
+        .version("1.3")
         .author("Sean Cross <sean@xobs.io>")
         .about("Takes compiled code and modulates it for a Love-to-Code sticker")
         .arg(
@@ -191,6 +221,14 @@ fn main() {
                 .help("Data protocol version"),
         )
         .arg(
+            Arg::with_name("repeats")
+                .short("c")
+                .long("repeat-count")
+                .value_name("COUNT")
+                .takes_value(true)
+                .default_value("3")
+                .help("Number of times to repeat"),
+        )        .arg(
             Arg::with_name("update")
                 .short("u")
                 .long("update")
@@ -213,6 +251,7 @@ fn main() {
     let target_filename = matches.value_of("output").unwrap_or("output.wav");
     let os_update = matches.is_present("update");
     let play_file = matches.is_present("play");
+    let repeats = matches.value_of("repeats").unwrap().parse::<u32>().unwrap();
     let protocol_version = match matches.value_of("version") {
         Some("1") => controller::ProtocolVersion::V1,
         Some("2") => controller::ProtocolVersion::V2,
@@ -242,6 +281,7 @@ fn main() {
         os_update,
         protocol_version,
         play_file,
+        repeats,
     ) {
         println!("Unable to modulate: {}", &err);
         std::process::exit(1);
